45f081fa68ff4b7c49db271014810c36d4a593a8
fanhaipeng <fanhaipeng0403@gmail.com>
2018-11-17 21:54:40 +0800

    用户相关表单

--- saleor/account/forms.py
+++ saleor/account/forms.py
@@ -1,15 +1,27 @@
 from captcha.fields import ReCaptchaField
+###基本表单
+###########################################################################
 from django import forms
+###########################################################################
 from django.conf import settings
-from django.contrib.auth import forms as django_forms, update_session_auth_hash
+## 用户相关的表单
+###########################################################################
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm as rawPasswordResetForm
 from django.utils.translation import pgettext, pgettext_lazy
 from phonenumbers.phonenumberutil import country_code_for_region
 
 from . import emails
-from ..account.models import User
 from .i18n import AddressMetaForm, get_address_form_class
+from ..account.models import User
 
 
+
+
+
+
+###一个django插件，使得操作的时候带有验证码
+###############################################
 class FormWithReCaptcha(forms.BaseForm):
     def __new__(cls, *args, **kwargs):
         if settings.RECAPTCHA_PUBLIC_KEY and settings.RECAPTCHA_PRIVATE_KEY:
@@ -20,62 +32,7 @@ class FormWithReCaptcha(forms.BaseForm):
         return super(FormWithReCaptcha, cls).__new__(cls)
 
 
-def get_address_form(
-        data, country_code, initial=None, instance=None, **kwargs):
-    country_form = AddressMetaForm(data, initial=initial)
-    preview = False
-    if country_form.is_valid():
-        country_code = country_form.cleaned_data['country']
-        preview = country_form.cleaned_data['preview']
-
-    if initial is None and country_code:
-        initial = {}
-    if country_code:
-        initial['phone'] = '+{}'.format(country_code_for_region(country_code))
-
-    address_form_class = get_address_form_class(country_code)
-
-    if not preview and instance is not None:
-        address_form_class = get_address_form_class(instance.country.code)
-        address_form = address_form_class(data, instance=instance, **kwargs)
-    else:
-        initial_address = (
-            initial if not preview
-            else data.dict() if data is not None else data)
-        address_form = address_form_class(
-            not preview and data or None,
-            initial=initial_address,
-            **kwargs)
-    return address_form, preview
-
-
-class ChangePasswordForm(django_forms.PasswordChangeForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['new_password1'].user = self.user
-        self.fields['old_password'].widget.attrs['placeholder'] = ''
-        self.fields['new_password1'].widget.attrs['placeholder'] = ''
-        del self.fields['new_password2']
-
-
-def logout_on_password_change(request, user):
-    if (update_session_auth_hash is not None and
-            not settings.LOGOUT_ON_PASSWORD_CHANGE):
-        update_session_auth_hash(request, user)
-
-
-class LoginForm(django_forms.AuthenticationForm, FormWithReCaptcha):
-    username = forms.EmailField(
-        label=pgettext('Form field', 'Email'), max_length=75)
-
-    def __init__(self, request=None, *args, **kwargs):
-        super().__init__(request=request, *args, **kwargs)
-        if request:
-            email = request.GET.get('email')
-            if email:
-                self.fields['username'].initial = email
-
-
+##注册
 class SignupForm(forms.ModelForm, FormWithReCaptcha):
     password = forms.CharField(
         widget=forms.PasswordInput,
@@ -106,7 +63,31 @@ class SignupForm(forms.ModelForm, FormWithReCaptcha):
         return user
 
 
-class PasswordResetForm(django_forms.PasswordResetForm, FormWithReCaptcha):
+##登录
+class LoginForm(AuthenticationForm, FormWithReCaptcha):
+    username = forms.EmailField(
+        label=pgettext('Form field', 'Email'), max_length=75)
+
+    def __init__(self, request=None, *args, **kwargs):
+        super().__init__(request=request, *args, **kwargs)
+        if request:
+            email = request.GET.get('email')
+            if email:
+                self.fields['username'].initial = email
+
+
+#修改密码
+class ChangePasswordForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['new_password1'].user = self.user
+        self.fields['old_password'].widget.attrs['placeholder'] = ''
+        self.fields['new_password1'].widget.attrs['placeholder'] = ''
+        del self.fields['new_password2']
+
+
+#重置密码
+class PasswordResetForm(rawPasswordResetForm, FormWithReCaptcha):
     """Allow resetting passwords.
 
     This subclass overrides sending emails to use templated email.
@@ -117,10 +98,46 @@ class PasswordResetForm(django_forms.PasswordResetForm, FormWithReCaptcha):
         return active_users
 
     def send_mail(
-            self, subject_template_name, email_template_name, context,
-            from_email, to_email, html_email_template_name=None):
+        self, subject_template_name, email_template_name, context,
+        from_email, to_email, html_email_template_name=None):
         # Passing the user object to the Celery task throws an
         # error "'User' is not JSON serializable". Since it's not used in our
         # template, we remove it from the context.
         del context['user']
         emails.send_password_reset_email.delay(context, to_email)
+
+
+###############################################
+def get_address_form(
+    data, country_code, initial=None, instance=None, **kwargs):
+    country_form = AddressMetaForm(data, initial=initial)
+    preview = False
+    if country_form.is_valid():
+        country_code = country_form.cleaned_data['country']
+        preview = country_form.cleaned_data['preview']
+
+    if initial is None and country_code:
+        initial = {}
+    if country_code:
+        initial['phone'] = '+{}'.format(country_code_for_region(country_code))
+
+    address_form_class = get_address_form_class(country_code)
+
+    if not preview and instance is not None:
+        address_form_class = get_address_form_class(instance.country.code)
+        address_form = address_form_class(data, instance=instance, **kwargs)
+    else:
+        initial_address = (
+            initial if not preview
+            else data.dict() if data is not None else data)
+        address_form = address_form_class(
+            not preview and data or None,
+            initial=initial_address,
+            **kwargs)
+    return address_form, preview
+
+
+def logout_on_password_change(request, user):
+    if (update_session_auth_hash is not None and
+        not settings.LOGOUT_ON_PASSWORD_CHANGE):
+        update_session_auth_hash(request, user)
